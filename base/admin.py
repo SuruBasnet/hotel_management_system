8c7ea842738b278da5287ea287f6351ac1f27c1e
SuruBasnet <surubasnet824@gmail.com>
2023-07-02 08:35:22 +0545

    changed admin to see data and tables in admin panel

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(GuestProfile)
+admin.site.register(GuestOrganization)
+admin.site.register(RoomType)
+admin.site.register(RoomService)
+admin.site.register(RoomInfo)
+admin.site.register(RoomInfoService)
+admin.site.register(CustomerProfile)
+admin.site.register(Bill)
+admin.site.register(CustomerPayment)
+admin.site.register(Booking)
+admin.site.register(Menu)
+admin.site.register(MenuType)
