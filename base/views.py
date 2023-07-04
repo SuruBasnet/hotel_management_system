bba8f95870e009597c0180ef388c70f82ffd4202
SuruBasnet <surubasnet824@gmail.com>
2023-07-04 08:52:46 +0545

    changes

--- base/views.py
+++ base/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
+
+def home(request):
+    return render(request,'index.html')
+
+def room(request):
+    room_objects = RoomInfo.objects.all()
+    content = {
+        'room' : room_objects
+    }
+
+    return render(request,'room.html',context=content)
