ff53da3096fdb5c67253bd262c47bf4e27e8cf77
SuruBasnet <surubasnet824@gmail.com>
2023-07-06 12:25:37 +0545

    added class based view

--- base/urls.py
+++ base/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     # path("home/",home,name='home'),
     # path("room/",room,name='room')
 
-    path('guest_profile/all/',guest_profile_apiview,name='guest_profile_list')
+    path('guest_profile/<str:pk>/',GuestProfileApiView.as_view(),name='guest_profile_list')
 ]
--- base/views.py
+++ base/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
-# from rest_framework import generics
-from rest_framework.decorators import api_view
+from rest_framework import generics # For making class view
+from rest_framework.decorators import api_view # For making function views
 from rest_framework.response import Response
 from .serializers import *
 
@@ -23,8 +23,20 @@ from .serializers import *
 
 # While working with api we create views and url
 
-@api_view(['GET'])
-def guest_profile_apiview(request):
-    guestprofile_objects = GuestProfile.objects.all()
-    serializer = GuestProfileSerializer(guestprofile_objects,many=True)
-    return Response(serializer.data)
+# JSON - {"username":"ram","password":"ram123"}
+
+# @api_view(['GET'])
+# def guest_profile_apiview(request):
+#     guestprofile_objects = GuestProfile.objects.all()
+#     serializer = GuestProfileSerializer(guestprofile_objects,many=True)
+#     return Response(serializer.data)
+
+
+class GuestProfileApiView(generics.GenericAPIView):
+    queryset = GuestProfile
+    serializer_class = GuestProfileSerializer
+
+    def get(self,request,pk=None):
+        guest_data = GuestProfile.objects.get(guest_name=pk)
+        serializer = self.serializer_class(guest_data)
+        return Response(serializer.data)
